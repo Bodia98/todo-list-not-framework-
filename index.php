31506d351a6eebbc88e89bbc39f9ffc7df6d5331
Bodia98 <oliynykbogdan.dev@gmail.com>
2019-08-08 18:50:40 +0300

    php and add function to db

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+require_once 'app/init.php';
+
+$itemsQuery = $db->prepare("
+    SELECT id, name, done
+    FROM items
+    WHERE user = :user
+");
+
+$itemsQuery->execute([
+    'user' => $_SESSION['user_id']
+]);
+
+
+$items = $itemsQuery->rowCount() ? $itemsQuery : [];
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,18 +29,21 @@
 <body>
 <div class="list">
     <h1 class="header">TO do.</h1>
-    <ul class="items">
 
-        <li>
-            <span class="item">Pick up shopping</span>
-            <a href="#" class="done-button">Mark as done</a>
-        </li>
-        <li>
-            <span class="item done">Learn php</span>
-        </li>
-
-    </ul>
-
+    <?php if (!empty($items)): ?>
+        <ul class="items">
+            <?php foreach ($items as $item): ?>
+                <li>
+                    <span class="item<?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name'];?></span>
+                    <?php if (!$item['done']): ?>
+                        <a href="#" class="done-button">Mark as done</a>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p>You haven`t added any items yet</p>
+    <?php endif; ?>
     <form class="item-add" action="add.php" method="post">
         <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
         <input type="submit" value="Add" class="submit">
